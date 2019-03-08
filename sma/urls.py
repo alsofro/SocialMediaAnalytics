1b39fbd5b6d37a64c9def5cc518f4cc97c611692
reberu <redlightexit@gmail.com>
2019-03-09 01:06:01 +0900

    Добавлено приложение authapp

--- sma/urls.py
+++ sma/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 import mainapp.views as mainapp
 
 urlpatterns = [
     path('', mainapp.main),
     path('admin/', admin.site.urls),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
