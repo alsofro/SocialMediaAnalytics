41c796a42e3420b52ca528b2df30e0cbfbb3ed3e
Aleksandr <ynxela@gmail.com>
2019-03-18 01:23:31 +0300

    Добавил приложение для поиска людей
    Приложение позволяет искать общих пользователей двух групп. Пока что 2 группы, планирую сделать метод универсальным, чтобы принимал n количество аргументов. Работает медленно, тестируйте на небольших группах. Например, до 10000 человек. Принимает в себя id, либо короткое название, которое идет в url группы после vk.com/

--- dataqueryapp/urls.py
+++ dataqueryapp/urls.py
@@ -5,5 +5,6 @@ app_name = 'dataqueryapp'
 
 urlpatterns = [
     path('test/', dataqueryapp.test, name='test'),
+    path('similar_members/', dataqueryapp.similar_members, name='similar_members'),
     path('', dataqueryapp.main, name='main'),
 ]
